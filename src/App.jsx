0adf86b0637398cfe3c5fef26705c6eb08328dad
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-05-26 18:28:59 +0100

    Added transactions and logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,34 @@ function App() {
   const [income, setIncome] = useState(0);
   const [expenses, setExpenses] = useState(0);
   const [amount, setAmount] = useState(0);
+  const [transactions, setTransactions] = useState([]);
 
   const balance = income - expenses;
 
-  const click = () => {
+  const handleAddIncome = () => {
+    if(amount <= 0){ return}
     setIncome(income + amount)
-    setAmount('');
+    setTransactions([...transactions, {
+      id: Date.now(),
+      type: "income",
+      amount: amount
+    }]);
+    setAmount(0);
   }
 
-  const clickExpense = () => {
+  const handleAddExpense = () => {
+    if(amount <= 0 || amount > balance){ return}
     setExpenses(expenses + amount);
-    setAmount('');
+    setTransactions([...transactions, {
+      id: Date.now(),
+      type: "expense",
+      amount: amount
+    }]);
+    setAmount(0);
+  }
 
+  const remove = (id) => {
+    setTransactions(transactions.filter(item => item.id != id))
   }
 
   return (
@@ -28,8 +44,14 @@ function App() {
       <Card title="Income" amount={income} />
       <Card title="Expenses" amount={expenses} />
       <input type="number" placeholder='Enter an amount' value={amount} onChange={(e) => setAmount(Number(e.target.value)) } />
-      <button onClick={click}>Add income</button>
-      <button onClick={clickExpense}>Add expense</button>
+      <button onClick={handleAddIncome}>Add income</button>
+      <button onClick={handleAddExpense}>Add expense</button>
+      
+      <ul>
+        {transactions.map(item => 
+          <li key={item.id}>{item.type} - {item.amount} <button onClick={() => remove(item.id)}>delete</button></li>
+        )}
+      </ul>
       
     </div>
   )
